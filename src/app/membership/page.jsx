1e63b2310ed9cb92aec425c572c31b4ab0179087
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-06-15 08:06:08 +0530

    added new files

---
+++ src/app/membership/page.jsx
@@ -0,0 +1,82 @@
+'use client'
+import React from "react";
+import logo from "../../../public/Seal_of_Sikkim.png"
+import Image from "next/image";
+import Link from "next/link";
+
+function Page() {
+    const [email, setEmail] = React.useState('')
+    const [password, setPassword] = React.useState('')
+    const[first_name,setFirstName] = React.useState('')
+    const[last_name,setLastName] = React.useState('')
+
+    const handleForm = async (event) => {
+        event.preventDefault()
+
+        const { result, error } = await signUp(email, password,first_name);
+
+        if (error) {
+            return console.log(error)
+        }
+
+        // else successful
+        console.log(result)
+        return router.push("/dashboard")
+    }
+
+        return (
+        <section class="bg-gray-50 dark:bg-gray-900">
+  <div class="flex flex-col items-center justify-center px-7 py-7 mx-auto md:h-screen lg:py-0">
+      <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+        <Image src={logo} height={49} width ={49} alt="Seal of Sikkim"/>
+      </a>
+      <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+          <div class="p-7 space-y-4 md:space-y-6 sm:p-8">
+              <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                  Enter your details 
+              </h1>
+              <form class="space-y-9 md:space-y-6" onSubmit={handleForm}>
+                  <div>
+                      <label for="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                      <input type="email" name="email" id="email" value ={email} onChange={(e)=>setEmail(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-green-500 focus:border-green-400 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required/>
+                  </div>
+                  <div>
+                      <label for="text" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your Full Name</label>
+                      <input type="text" name="fullname" id="name" value={first_name} onChange={(e)=>setFirstName(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-green-500 focus:border-green-400 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter your full name" required/>
+                  </div>
+
+
+
+
+                  <div>
+                      <label for="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                      <input type="password" name="password" id="password" value={password} onChange={(e)=>setPassword(e.target.value)} placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required/>
+                  </div>
+                  <div>
+                      <label for="confirm-password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm password</label>
+                      <input type="confirm-password" name="confirm-password" id="confirm-password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required/>
+                  </div>
+                  <div className="flex items-start">
+                      <div className="flex items-center h-5">
+                        <input id="terms" aria-describedby="terms" type="checkbox" className="w-4 h-4 border border-green-300 rounded bg-gray-50 focus:ring-green focus:ring-green-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-green-500 dark:ring-offset-gray-800" required/>
+                      </div>
+                      <div className="ml-3 text-sm">
+                        <label for="terms" class="font-light text-gray-500 dark:text-gray-300">I accept the <Link className="font-medium text-green-500 hover:underline dark:text-primary-500" href="#">Terms and Conditions</Link></label>
+                      </div>
+                  </div>
+                  <button type="submit" className="w-full text-white bg-green-500 hover:bg-primary-700 focus:ring-green-500 focus:outline-green-500 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-green-500 dark:focus:ring-primary-800">Create an account</button>
+                  <p class="text-sm font-light text-gray-500 dark:text-gray-400">
+                      Already have an account? <Link href="/signin" className="font-medium text-green-500 hover:underline dark:text-primary-500">Login here</Link>
+                  </p>
+              </form>
+          </div>
+      </div>
+  </div>
+</section>
+    );
+
+   
+
+}
+
+export default Page;
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -9,7 +9,7 @@ export default function Home() {
         <div className="mr-auto place-self-center lg:col-span-7">
             <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">Online Voting System for Elderly Voters</h1>
             <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">Vote cast for all <strong className="font-semibold">Citizens of Sikkim state strictly above 75 years</strong> casting their vote in  2024 vote  can be done via this portal.Click the PROCEED button to continue </p>
-            <Link href="/signup" className="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-green-500 hover:bg-primary-800 focus:ring-4 focus:ring-green-400 dark:focus:ring-primary-900">
+            <Link href="/membership" className="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-green-500 hover:bg-primary-800 focus:ring-4 focus:ring-green-400 dark:focus:ring-primary-900">
                 Proceed
                 <svg class="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
             </Link>
